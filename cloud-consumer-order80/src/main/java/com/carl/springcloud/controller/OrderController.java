0f1c4fee5813210dc3408688043966181e7e0f99
Carl <carln@foxmail.com>
2021-04-15 16:50:19 +0100

    利用Eureka集群实现注册，actuator完善服务信息

--- cloud-consumer-order80/src/main/java/com/carl/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/carl/springcloud/controller/OrderController.java
@@ -18,7 +18,10 @@ import javax.annotation.Resource;
 @RestController
 @Slf4j
 public class OrderController {
-    public static final String PAYMENT_URL = "http://localhost:8001";
+    //为了实现负载均衡，此处URL不能写死
+    //public static final String PAYMENT_URL = "http://localhost:8001";
+    public static final String PAYMENT_URL = "http://CLOUD-PAYMENT-SERVICE";
+
     @Resource
     private RestTemplate restTemplate;
 
--- cloud-provider-payment8001/src/main/java/com/carl/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/carl/springcloud/controller/PaymentController.java
@@ -4,9 +4,13 @@ import com.carl.springcloud.entities.CommonResult;
 import com.carl.springcloud.entities.Payment;
 import com.carl.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @Auther: Carl
@@ -19,12 +23,18 @@ public class PaymentController {
     @Resource
     private PaymentService paymentService;
 
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
+
     @PostMapping(value = "/payment/create")
     public CommonResult<Payment> create(@RequestBody Payment payment) {
         int result = paymentService.create(payment);
         log.info("---插入结果" + payment);
         if (result > 0)  {
-            return new CommonResult(200, "插入成功", result);
+            return new CommonResult(200, "插入成功，server port: " + serverPort, result);
         } else {
             return new CommonResult(444, "插入失败", null);
         }
@@ -35,9 +45,25 @@ public class PaymentController {
         Payment payment = paymentService.getPaymentById(id);
         log.info("---查询结果" + payment);
         if (payment != null)  {
-            return new CommonResult(200, "查询成功啦！", payment);
+            return new CommonResult(200, "查询成功啦，server port" + serverPort, payment);
         } else {
             return new CommonResult(444, "无对应ID：" + id, null);
         }
+    }
+
+    /**
+     * 获取注册服务的详细信息
+     */
+    @GetMapping(value = "/payment/discovery")
+    public Object discovery(){
+        List<String> services = discoveryClient.getServices();
+        for (String elements : services) {
+            log.info("element: " + elements);
+        }
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for (ServiceInstance serviceInstance : instances) {
+            log.info(serviceInstance.getInstanceId() + "\t" + serviceInstance.getHost() + "\t" + serviceInstance.getPort() + "\t" + serviceInstance.getUri());
+        }
+        return this.discoveryClient;
     }
 }
