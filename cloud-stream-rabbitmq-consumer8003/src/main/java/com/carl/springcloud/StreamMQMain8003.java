3e61e4c32c21e96dac699a9f2494430add9c0746
Carl <carln@foxmail.com>
2021-04-22 13:50:47 +0100

    消息总线RabbitMQ  消费者

---
+++ cloud-stream-rabbitmq-consumer8003/src/main/java/com/carl/springcloud/StreamMQMain8003.java
@@ -0,0 +1,17 @@
+package com.carl.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Auther: Carl
+ * @Date: 2021/04/21/15:19
+ * @Description:
+ */
+@SpringBootApplication
+
+public class StreamMQMain8003 {
+     public static void main(String[] args) {
+           SpringApplication.run(StreamMQMain8003.class, args);
+      }
+}
